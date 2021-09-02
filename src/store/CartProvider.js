fd8daaa4f8060ea14bc5b78fbe6737bb142d43b7
Lirim Kusari <kusarilirim@gmail.com>
2021-09-02 18:09:51 +0200

    idk

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,30 @@
 import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defalutCardState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defalutCardState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+  const [cartState, dispatchCardAction] = useReducer(
+    cartReducer,
+    defalutCardState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCardAction({ type: "ADD", item: item });
+  };
 
   const removeItemFromCartHandler = (item) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartContext.items,
+    totalAmount: cartState,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
